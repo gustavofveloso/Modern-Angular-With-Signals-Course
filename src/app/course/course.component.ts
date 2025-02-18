b97e508eccab5b13c44eaf4537d75175d6c89fb4
Gustavo Carvalho <gustavo.carvalho@meioambiente.mg.gov.br>
2025-02-18 17:17:05 -0300

    API of service layer

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { Course } from '../models/course.model';
+import { CoursesService } from '../services/courses.service';
 
 @Component({
   selector: 'course',
   standalone: true,
   imports: [],
   templateUrl: './course.component.html',
-  styleUrl: './course.component.scss'
+  styleUrl: './course.component.scss',
 })
 export class CourseComponent {
+  courses = signal<Course[]>([]);
 
+  coursesService = inject(CoursesService);
 }
